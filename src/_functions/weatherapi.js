db028de0574766bdee4801eed2a0900f0ee23cfb
zsaurabhprdxn <saurabhz@prdxn.com>
2021-05-11 18:42:58 +0530

    context parameter added

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-exports.handler = async function(event, callback) {
+exports.handler = async function(event, context, callback) {
 
     const { lat, lon} = event.queryStringParameters;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_KEY}&units=metric`;
