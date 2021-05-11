769bd40a772ffa8fe87d34a711348b8b39c3012d
zsaurabhprdxn <saurabhz@prdxn.com>
2021-05-11 12:53:15 +0530

    redirects applied

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,26 @@
-const axios = require('axios');
 require('dotenv').config();
+const axios = require('axios');
+const countries = require('./countries.json');
 
-module.exports = async function() {
+async function getNews(country) {
     try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}`);
-        return response.data;
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}`);
+        return {
+            "country":country,
+            "articles": response.data.articles
+        }; 
     } catch (error) {
         console.log(error);
     }
+};
+
+
+module.exports = async function() {
+   
+    const newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then( newsObj => {
+        // return [].concat.apply([],newsObj);
+        return newsObj;
+
+    })
 };
